1d35b04176b4670f37882f23709d76bf63919512
Advait Junnarkar <advaitju@users.noreply.github.com>
2020-09-09 16:44:46 +1000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	extends: ['airbnb', 'prettier', 'prettier/react'],
+	plugins: ['prettier'],
+	parser: 'babel-eslint',
+	rules: {
+		'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'lf',
+				singleQuote: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'es5',
+			},
+		],
+	},
+};
